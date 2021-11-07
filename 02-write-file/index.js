a776fc34235b1c8d6fb703162408bdb4f60e53d2
Pas92 <pavelburdavitsyn92@gmail.com>
2021-11-07 16:30:58 +0300

    feat: add solution for task '02-write-file'

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const{ stdin, stdout } = process;
+const path = require('path');
+const fs = require('fs');
+const output = fs.createWriteStream(path.join(__dirname, 'stream-file.txt'));
+
+stdout.write('Введите текст: ');
+
+const flag = 'exit\r\n';
+
+stdin.on('data', data => {
+  console.log(Buffer.from(flag));
+  console.log(Buffer.from(data));
+  if (data.toString() === flag) {
+    process.exit();
+  } else {
+    output.write(data);
+    stdout.write('Введите текст: ');
+  }
+});
+
+process.on('exit', () => stdout.write('Ввод данных завершен'));
+process.on('SIGINT', () => {
+  stdout.write('\r\n');
+  process.exit();
+});
+
+
