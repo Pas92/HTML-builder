e5bf7bddf8c6c5a019adaf697f9a35dd42d0ac68
Pas92 <pavelburdavitsyn92@gmail.com>
2021-11-07 23:19:02 +0300

    feat: add solution for task '04-copy-directory'

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,39 @@
+const path = require('path');
+const fs = require('fs');
+
+
+const folderPath = path.join(__dirname, 'files');
+const targetFolder = path.join(__dirname, 'files-copy');
+
+fs.readdir(targetFolder, (err, files) => {
+  if (err) throw err;
+
+  for(file of files) {
+    let target = file
+    fs.unlink(path.join(targetFolder, target.toString()), err => {
+      if(err) throw err;
+      console.log('Файл успешно удалён');
+    });
+  }
+});
+
+fs.mkdir(targetFolder, { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+fs.readdir(folderPath, (err, files) => {
+  if (err) throw err;
+
+  for(file of files) {
+    let target = file
+    console.log(target);
+    fs.copyFile(path.join(folderPath, target.toString()), path.join(targetFolder, target.toString()), err => {
+      if(err) throw err;
+      console.log('Файл успешно скопирован');
+    });
+  }
+});
+
+
+
 
