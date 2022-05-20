a4c3a713bd0797a10cfb54980dc18c998f4f6219
Pas92 <pavelburdovitsin92@gmail.com>
2022-05-20 15:33:47 +0300

    fix: fix exercise 4 using async functions

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,39 +1,39 @@
 const path = require('path');
 const fs = require('fs');
+const fsPromises = require('fs/promises');
 
 
 const folderPath = path.join(__dirname, 'files');
 const targetFolder = path.join(__dirname, 'files-copy');
 
-fs.readdir(targetFolder, (err, files) => {
-  if (err) throw err;
-
-  for(file of files) {
-    let target = file
-    fs.unlink(path.join(targetFolder, target.toString()), err => {
-      if(err) throw err;
-      console.log('Файл успешно удалён');
-    });
+const createFolder = async () => {
+  try {
+    await fsPromises.mkdir(targetFolder, { recursive: true });
+  } catch (error) {
+    console.log(error)
   }
-});
+}
 
-fs.mkdir(targetFolder, { recursive: true }, (err) => {
-  if (err) throw err;
-});
-
-fs.readdir(folderPath, (err, files) => {
-  if (err) throw err;
-
-  for(file of files) {
-    let target = file
-    console.log(target);
-    fs.copyFile(path.join(folderPath, target.toString()), path.join(targetFolder, target.toString()), err => {
-      if(err) throw err;
-      console.log('Файл успешно скопирован');
-    });
+const copyFiles = async () => {
+  try {
+    const files = await fsPromises.readdir(folderPath);
+    for(file of files) {
+      let target = file
+      try {
+        await fsPromises.copyFile(path.join(folderPath, target.toString()), path.join(targetFolder, target.toString()));
+        console.log(`Файл ${target} успешно скопирован из папки ${folderPath} в папку ${targetFolder}`);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  } catch (error) {
+    console.log(error)
   }
-});
+}
 
+const copyBetweenFolders = async () => {
+  await createFolder();
+  await copyFiles();
+}
 
-
-
+copyBetweenFolders()
