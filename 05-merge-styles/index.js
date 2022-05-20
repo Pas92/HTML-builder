3b127439e302e60bb93bea67ceef81dc03911c53
Pas92 <pavelburdovitsin92@gmail.com>
2022-05-20 15:59:34 +0300

    fix: fix exercise 5 using async functions

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,25 +1,28 @@
 const path = require('path');
 const fs = require('fs');
-
+const fsPromises = require('fs/promises');
 
 const folderPath = path.join(__dirname, 'styles');
 const targetFolder = path.join(__dirname, 'project-dist');
 
-fs.readdir(folderPath, (err, files) => {
-  if (err) throw err;
-  const output = fs.createWriteStream(path.join(targetFolder, 'bundle.css'));
-
-  for(file of files) {
-    let target = file
-    // console.log(target);
-
-    if(path.extname(target) === '.css') {
-      const stream = fs.createReadStream(path.join(folderPath, target.toString()), 'utf-8');
-      let data = '';
-      stream.on('data', chunk => data += chunk);
-      stream.on('end', () => {
-        output.write(data);
-      });
+const createBundle = async (source, bundleName, bundleExtension) => {
+  try {
+    const files = await fsPromises.readdir(source);
+    const output = fs.createWriteStream(path.join(targetFolder, `${bundleName}.${bundleExtension}`));
+    for(file of files) {
+      let target = file
+      if(path.extname(target) === `.${bundleExtension}`) {
+        const stream = fs.createReadStream(path.join(folderPath, target.toString()), 'utf-8');
+        let data = '';
+        stream.on('data', chunk => data += chunk);
+        stream.on('end', () => {
+          output.write(data);
+        });
+      }
     }
+  } catch (error) {
+    console.log(error)
   }
-});
+}
+
+createBundle(folderPath, 'bundle', 'css')
