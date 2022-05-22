7663f401971863d8ed67e95451243079eec08c69
Pas92 <pavelburdavitsyn92@gmail.com>
2022-05-22 16:23:34 +0300

    fix: fix 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const fs = require('fs');
 const fsPromises = require('fs/promises');
 
 
@@ -10,30 +9,43 @@ const createFolder = async () => {
   try {
     await fsPromises.mkdir(targetFolder, { recursive: true });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
+
+const deleteFiles = async () => {
+  try {
+    const files = await fsPromises.readdir(targetFolder);
+    for(let file of files) {
+      let target = file;
+      fsPromises.unlink(path.join(targetFolder, target));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const copyFiles = async () => {
   try {
     const files = await fsPromises.readdir(folderPath);
-    for(file of files) {
-      let target = file
+    for(let file of files) {
+      let target = file;
       try {
         await fsPromises.copyFile(path.join(folderPath, target.toString()), path.join(targetFolder, target.toString()));
         console.log(`Файл ${target} успешно скопирован из папки ${folderPath} в папку ${targetFolder}`);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 const copyBetweenFolders = async () => {
   await createFolder();
+  await deleteFiles();
   await copyFiles();
-}
+};
 
-copyBetweenFolders()
+copyBetweenFolders();
